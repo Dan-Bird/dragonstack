aaa188ffd3e143bf21849203ba1a6042d6c520da
Unknown <info@daniel-bird.co.uk>
2019-05-19 16:10:17 +0100

    Added Dragon Class
    Dragon Class has been added and refined to use default values if no properties are specified upon creation.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log('Hello Node');
+const Dragon = require('./dragon.js');
+
+const fooey = new Dragon({
+  birthdate: new Date(),
+  nickname: 'fooey'
+});
+
+const baloo = new Dragon({
+  birthdate: new Date(),
+  nickname: 'baloo'
+});
+
+const mimar = new Dragon();
+
+setTimeout(() => {
+  const gooby = new Dragon();
+  console.log('gooby', gooby);
+}, 3000);
+
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
+console.log('mimar', mimar);
+
