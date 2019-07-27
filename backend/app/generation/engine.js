bd6d226b852f68a3697f9a2488a0688da8ddc828
Unknown <info@daniel-bird.co.uk>
2019-07-27 21:19:31 +0100

    Added Functionality to Add Generations to Database
    Added a table.js file to create a namespace to add Generations to the Generations database.
    
    Also amended engine;js to log generations to the database each time a new generation is made.

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -1,4 +1,5 @@
 const Generation = require('./index');
+const GenerationTable = require('./table');
 
 class GenerationEngine {
   constructor() {
@@ -16,6 +17,8 @@ class GenerationEngine {
 
   buildNewGeneration() {
     this.generation = new Generation();
+
+    GenerationTable.storeGeneration(this.generation);
 
     console.log('new generation', this.generation);
 
--- backend/databasePool.js
+++ backend/databasePool.js
@@ -4,3 +4,13 @@ const databaseConfiguration = require('./secrets/databaseConfiguration');
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+/* TESTING SETUP -------------
+
+pool.query('SELECT foo FROM generation', (error, response) => {
+  if (error) return console.log('error', error);
+
+  console.log('response.rows', response.rows);
+});
+
+*/
