3929e711b91373786da25f2e712d130562c88351
Unknown <info@daniel-bird.co.uk>
2019-09-07 17:55:21 +0100

    Frontend & Backend Services Run with Single Script

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Generation from './components/Generation';
 
 render(
-  <div>Dragon Stack from React</div>,
+  <div>
+  <h2>Dragon Stack from React</h2>
+  <Generation />
+  </div>,
   document.getElementById('root')
 );
